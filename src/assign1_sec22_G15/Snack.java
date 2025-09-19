7d99ddf80734fbd17d4d5c5cf37566544c7cf001
alitanbooz <alitanbooz@gmail.com>
2025-09-19 18:49:17 +0400

    Add files via upload

---
+++ src/assign1_sec22_G15/Snack.java
@@ -0,0 +1,12 @@
+package assign1_sec22_G15;
+
+public class Snack extends Meal {
+    public Snack(String mealName, double calories, double protein, double carbohydrates, double fats) {
+        super(mealName, calories, protein, carbohydrates, fats);
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("Snack: " + mealName + " " + calories + " calories, " + protein + "g protein, " + carbohydrates + "g carbohydrates, " + fats + "g fats");
+    }
+}
