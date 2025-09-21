20bfdb598a523675f7c14d9dc17719d75f109336
eduardo <horstmanneduardo100@gmail.com>
2025-09-21 16:21:03 +0400

    fix: global scanner object fix: put methods in order create: weeklyMeals ArrayList to store DailyMeals modify: ViewNutritionProgress now compares DailyMeals in weeklyMeals to the nutritioGoal modify: GenerateNutritionReports compares the last added DailyMeals to the NutritionGoal

--- src/NutriTrack.java
+++ src/NutriTrack.java
@@ -27,14 +27,15 @@ public class NutriTrack {
                     System.out.println("Thank you for using CSC301's NutriTrack (A Personal Nutrition Tracker App). Have a healthy day!");
             }
         } while (userChoice != 0);
+        input.close();
     }
-    
+    public static Scanner input = new Scanner(System.in);
     public static ArrayList<Meal> loggedBreakfasts = new ArrayList<>();
     public static ArrayList<Meal> loggedLunchs = new ArrayList<>();
     public static ArrayList<Meal> loggedDinner = new ArrayList<>();
     public static ArrayList<Meal> loggedSnacks = new ArrayList<>();
     public static NutritionGoals nutritionGoals = new NutritionGoals(0, 0, 0, 0);
-    public static DailyMeals dailyMeals;
+    public static ArrayList<DailyMeals> weeklyMeals = new ArrayList<>();
 
     public static void DisplayMenu() {
         System.out.println("------------------------------------------------------------");
@@ -53,7 +54,6 @@ public class NutriTrack {
     }
 
     public static int UserMenuChoice() {
-        Scanner input = new Scanner(System.in);
         int choice;
         do {
             System.out.println("Your Choice (0-8):");
@@ -62,8 +62,8 @@ public class NutriTrack {
         return choice;
     }
 
+    // 1. Log a new meal (Breakfast, Lunch, Dinner, Snack, ...)
     public static void LogNewMeal() {
-        Scanner input = new Scanner(System.in);
         int choice = -1;
         do {
             DisplayMealTypes();
@@ -110,11 +110,10 @@ public class NutriTrack {
             }
             input.nextLine();
         } while (choice != 0);
-        input.close();
     }
 
+    // 2. Update meal details (calories, protein, carbs, fats, etc.)
     public static void UpdateMeal() {
-        Scanner input = new Scanner(System.in);
         int choice = -1;
         do {
             DisplayMealTypes();
@@ -194,51 +193,10 @@ public class NutriTrack {
             }
             input.nextLine();
         } while (choice != 0);
-        input.close();
     }
 
-    public static void DisplayMealTypes() {
-        System.out.println("Select meal type:");
-        System.out.println("1. Breakfast");
-        System.out.println("2. Lunch");
-        System.out.println("3. Dinner");
-        System.out.println("4. Snack");
-    }
-
-    public static void ViewLoggedMeals() {
-        for (Meal meal : loggedBreakfasts) {
-            meal.displayMeal();
-        }
-        for (Meal meal : loggedLunchs) {
-            meal.displayMeal();
-        }
-        for (Meal meal : loggedDinner) {
-            meal.displayMeal();
-        }
-        for (Meal meal : loggedSnacks) {
-            meal.displayMeal();
-        }
-        Scanner input = new Scanner(System.in);
-        int choice;
-        do {
-            System.out.println("(0 to go back to the main menu, any other number to view logged meals):");
-            choice = input.nextInt();
-        } while (choice != 0);
-        input.close();
-    }
-
-    public static void SetUpdateDailyMeals() {
-        Scanner input = new Scanner(System.in);
-        int choice = -1;
-        do {
-            System.out.println("(0 to go back to the main menu, any other number to set or update current nutrition):");
-            choice = input.nextInt();
-        } while (choice != 0);
-        input.close();
-    }
-
+    // 3. Set or update nutrition goals.
     public static void SetUpdateNutritionGoals() {
-        Scanner input = new Scanner(System.in);
         int choice = -1;
         do {
             try {
@@ -262,84 +220,35 @@ public class NutriTrack {
             System.out.println("(0 to go back to the main menu, any other number to set or update nutrition goals):");
             choice = input.nextInt();
         } while (choice != 0);
-        input.close();
     }
-    
+
+    // 4. View progress toward nutrition goals.
     public static void ViewNutritionProgress() {
-    	Scanner input = new Scanner(System.in);
         int choice = -1;
         do {
             System.out.println("Your Progress");
-            System.out.println("Fats: " + dailyMeals.getFats() + "-" + nutritionGoals.getFats());
-            System.out.println("Protein: " + dailyMeals.getProtein() + "- " + nutritionGoals.getProtein());
-            System.out.println("Calories: " + dailyMeals.getCalories() + " -" + nutritionGoals.getCalories());
-            System.out.println("Carbs: " + dailyMeals.getCarbohydrates() +" -" + nutritionGoals.getCarbohydrates());
+            for(int i = 0; i < weeklyMeals.size(); i++) {
+                System.out.println("Day " + (i+1) + ":");
+                System.out.println("Calories towards goal: " + (weeklyMeals.get(i).getCalories() - nutritionGoals.getCalories()));
+                System.out.println("Protein towards goal: " + (weeklyMeals.get(i).getProtein() - nutritionGoals.getProtein()) + "g");
+                System.out.println("Carbohydrates towards goal: " + (weeklyMeals.get(i).getCarbohydrates() - nutritionGoals.getCarbohydrates()) + "g");
+                System.out.println("Fats towards goal: " + (weeklyMeals.get(i).getFats() - nutritionGoals.getFats()) + "g");
+            }
             System.out.println("(0 to go back to the main menu, any other number to view nutrition progress):");
-            choice = input.nextInt();
+            try {
+                choice = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("");
+            }
+            input.nextLine();
         } while (choice != 0);
-        input.close();
     }
-    
 
-    public static void GenerateNutritionReports1() {
-    	System.out.println("Nutrition Report :");
-         dailyMeals.getBreakfast().displayMeal();
-         dailyMeals.getLunch().displayMeal();
-         dailyMeals.getDinner().displayMeal();
-         dailyMeals.getSnack().displayMeal();
-    }
- 
-    public static void initializeDataForAdvice(){
-            
-        Scanner input = new Scanner(System.in);
-            double m;
-            double h;
-            
-        while (true){
-            System.out.println("Enter your Bodymass in kg: ");
-            if(input.hasNextDouble()) {
-                
-                m = input.nextDouble();
-                
-                if(m>0) break;
-            }else {
-                input.next();
-                }
-            System.out.println("Please enter a positive number.");
-    
-        }
-        
-        
-        while (true){
-            System.out.println("Enter your Height in cm: ");
-            
-            if(input.hasNextDouble()) {
-                
-                h = input.nextDouble();
-                
-                if(h>0) break;
-            }else {
-                input.next();
-                }
-            System.out.println("Please enter a positive number.");
-        }
-            
-            
-        MealAdvice advice = new MealAdvice(m,h);
-
-        advice.setBMI();
-            
-        System.out.printf("BMI: %.1f%n", advice.getBMI());
-        
-        System.out.println(advice.getAdvice());
-    
-        input.close();
-        
-    }
+    // 5. Generate nutrition reports (daily, weekly, monthly)
 	public static void GenerateNutritionReports() {
-        Scanner input = new Scanner(System.in);
         int choice = -1;
         do {
+            DailyMeals dailyMeals = weeklyMeals.get(weeklyMeals.size() - 1);
             if (dailyMeals.getBreakfast() == null || dailyMeals.getLunch() == null || dailyMeals.getDinner() == null || dailyMeals.getSnack() == null) {
                 System.out.println("Meals are not added yet.");
             }
@@ -387,11 +296,138 @@ public class NutriTrack {
                 System.out.println("==================================================");  
             }
             System.out.println("(0 to go back to the main menu, any other number to generate another report):");
+            try {
+                choice = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("");
+            }
+            input.nextLine();
+        } while (choice != 0);        
+    }
+
+    // 6. View logged meals.
+    public static void ViewLoggedMeals() {
+        for (Meal meal : loggedBreakfasts) {
+            meal.displayMeal();
+        }
+        for (Meal meal : loggedLunchs) {
+            meal.displayMeal();
+        }
+        for (Meal meal : loggedDinner) {
+            meal.displayMeal();
+        }
+        for (Meal meal : loggedSnacks) {
+            meal.displayMeal();
+        }
+        int choice;
+        do {
+            System.out.println("(0 to go back to the main menu, any other number to view logged meals):");
             choice = input.nextInt();
         } while (choice != 0);
+    }
+
+    // 7. Set or update daily meals.
+    public static void SetUpdateDailyMeals() {
+        int choice = -1;
+        do {
+            try {
+                System.out.println("Choose your day (enter the number): ");
+                int dayNumber;
+                for(dayNumber = 0; dayNumber <= weeklyMeals.size(); dayNumber++) {
+                    if(dayNumber < weeklyMeals.size()) System.out.println((dayNumber+1) + ". ");
+                    else System.out.println((dayNumber+1) + ". " + "Add a new day");
+                }      
+                dayNumber = input.nextInt() - 1;
+                if(dayNumber == weeklyMeals.size()) weeklyMeals.add(new DailyMeals(null, null, null, null));
+                
+                DailyMeals dailyMeals = weeklyMeals.get(dayNumber);
+
+                System.out.println("Choose your breakfast(enter the number): ");
+                for(int i = 0; i < loggedBreakfasts.size(); i++) System.out.println((i+1) + ". " + loggedBreakfasts.get(i).getMealName());      
+                int breakfastNumber = input.nextInt() - 1;
+                System.out.println("Choose your lunch(enter the number): ");
+                for(int i = 0; i < loggedLunchs.size(); i++) System.out.println((i+1) + ". " + loggedLunchs.get(i).getMealName());      
+                int lunchNumber = input.nextInt() - 1;
+                System.out.println("Choose your dinner(enter the number): ");
+                for(int i = 0; i < loggedDinner.size(); i++) System.out.println((i+1) + ". " + loggedDinner.get(i).getMealName());      
+                int dinnerNumber = input.nextInt() - 1;
+                System.out.println("Choose your snack(enter the number): ");
+                for(int i = 0; i < loggedSnacks.size(); i++) System.out.println((i+1) + ". " + loggedSnacks.get(i).getMealName());      
+                int snackNumber = input.nextInt() - 1;
+                dailyMeals.setBreakfast(loggedBreakfasts.get(breakfastNumber));
+                dailyMeals.setLunch(loggedLunchs.get(lunchNumber));
+                dailyMeals.setDinner(loggedDinner.get(dinnerNumber));
+                dailyMeals.setSnack(loggedSnacks.get(snackNumber));
+            } catch (Exception e) {
+                System.out.println("Invalid input");
+            }
+            System.out.println("(0 to go back to the main menu, any other number to set or update daily meals):");
+            try {
+                choice = input.nextInt();
+            } catch (Exception e) {
+                System.out.println("");
+            }
+            input.nextLine();
+        } while (choice != 0);
+    }
+
+    // 8. Get meal advice.
+    public static void initializeDataForAdvice(){ 
+            double m;
+            double h;
+            
+        while (true){
+            System.out.println("Enter your Bodymass in kg: ");
+            if(input.hasNextDouble()) {
+                
+                m = input.nextDouble();
+                
+                if(m>0) break;
+            }else {
+                input.next();
+                }
+            System.out.println("Please enter a positive number.");
+    
+        }
+        
+        
+        while (true){
+            System.out.println("Enter your Height in cm: ");
+            
+            if(input.hasNextDouble()) {
+                
+                h = input.nextDouble();
+                
+                if(h>0) break;
+            }else {
+                input.next();
+                }
+            System.out.println("Please enter a positive number.");
+        }
+            
+            
+        MealAdvice advice = new MealAdvice(m,h);
+
+        advice.setBMI();
+            
+        System.out.printf("BMI: %.1f%n", advice.getBMI());
+        
+        System.out.println(advice.getAdvice());
+    
         input.close();
         
     }
+
+    //helper method for displaying meal types
+    public static void DisplayMealTypes() {
+        System.out.println("Select meal type:");
+        System.out.println("1. Breakfast");
+        System.out.println("2. Lunch");
+        System.out.println("3. Dinner");
+        System.out.println("4. Snack");
+    }
+
+    //initialize default meals data
     private static void initializeDefaultMeals() {
         //default Breakfasts
         loggedBreakfasts.add(new Breakfast("Oatmeal with Berries", 350, 12, 60, 8));
@@ -420,41 +456,11 @@ public class NutriTrack {
         //default Nutrition Goals
         nutritionGoals = new NutritionGoals(2000, 150, 200, 50);
 
-        //default Current Nutrition
-        dailyMeals = new DailyMeals(loggedBreakfasts.get(0), loggedLunchs.get(0), loggedDinner.get(0), loggedSnacks.get(0));
+        //default Weekly Meals
+        weeklyMeals.add(new DailyMeals((Breakfast)loggedBreakfasts.get(0), (Lunch)loggedLunchs.get(0), (Dinner)loggedDinner.get(0), (Snack)loggedSnacks.get(0)));
+        weeklyMeals.add(new DailyMeals((Breakfast)loggedBreakfasts.get(1), (Lunch)loggedLunchs.get(1), (Dinner)loggedDinner.get(1), (Snack)loggedSnacks.get(1)));
+        weeklyMeals.add(new DailyMeals((Breakfast)loggedBreakfasts.get(2), (Lunch)loggedLunchs.get(2), (Dinner)loggedDinner.get(2), (Snack)loggedSnacks.get(2)));
+        weeklyMeals.add(new DailyMeals((Breakfast)loggedBreakfasts.get(3), (Lunch)loggedLunchs.get(3), (Dinner)loggedDinner.get(3), (Snack)loggedSnacks.get(3)));
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
